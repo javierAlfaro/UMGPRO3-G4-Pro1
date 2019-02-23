9ed5a383a777a0c1c7f48d79af05064c2ef7c588
javierAlfaro <47900381+javierAlfaro@users.noreply.github.com>
2019-02-22 23:28:18 -0600

    Create usuario.java

---
+++ usuario.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiendita;
+
+/**
+ *
+ * @author Javier Alfaro
+ */
+public class usuario {
+    String name;
+    String user;
+    String pass;
+    
+    public usuario(){
+        
+    }
+}
