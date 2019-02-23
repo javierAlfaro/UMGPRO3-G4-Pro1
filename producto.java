85234c2be432d58cd7472ddfa94cf67e7f8dda6c
javierAlfaro <47900381+javierAlfaro@users.noreply.github.com>
2019-02-22 23:27:57 -0600

    Create producto.java

---
+++ producto.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiendita;
+
+/**
+ *
+ * @author Javier Alfaro
+ */
+public class producto {
+    String id;
+    String nombre;
+    String descripcion;
+    int costo;
+    
+    public producto (){
+        
+    }
+}
